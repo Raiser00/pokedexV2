9444af96f4c5b6ed2a2d98ff8acf7aa4d303a129
Raiser00 <damien.khashan@ifosup.wavre.be>
2024-08-20 22:21:44 +0200

    config param typeusercontroller

--- app/Http/Controllers/TypeUserController.php
+++ app/Http/Controllers/TypeUserController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Type;
+
 use Illuminate\Http\Request;
 
 class TypeUserController extends Controller
 {
-    //
+    public function index()
+    {
+        $types = Type::all();
+
+        return view('typeuser.index', [
+            'type'=> $types,
+        ]);
+    }
+
+    public function show(Type $type)
+    {
+        $type = Type::find($type->id);
+
+        return view('typeuser.show', [
+            'type'=> $type,
+        ]);
+    }
 }
