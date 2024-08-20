6f38e727c72fd645980d9aae4f52ccb533d1dca8
Raiser00 <damien.khashan@ifosup.wavre.be>
2024-08-20 11:07:35 +0200

    ajout parametre model + seeder Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,19 @@ class Category extends Model
 {
     use HasFactory;
 
+    public const PHYSICAL = "physical";
+    public const SPECIAL = "special";
+    public const STATUS = "status";
+
+    public static function category()
+    {
+        return [
+            self::PHYSICAL,
+            self::SPECIAL,
+            self::STATUS,
+        ];
+    }
+
     public function attacks()
     {
         // 1 category peut etre attribuer à plusieurs attacks
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create([
+            'name' => 'PHYSICAL',
+            'imgLink' => '/storage/images/category/physical.png',
+        ]);
+
+        Category::create([
+            'name'=> 'SPECIAL',
+            'imgLink' => '/storage/images/category/special.png',
+
+        ]);
+
+        Category::create([
+            'name' => 'STATUS',
+            'imgLink'=> '/storage/images/category/status.png',
+        ]);
     }
 }
