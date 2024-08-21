a81275ef9589640d7b36062ab40a51c626a3b3c6
Raiser00 <damien.khashan@ifosup.wavre.be>
2024-08-21 22:58:59 +0200

    ajout query recherche

--- app/Http/Controllers/PokedexUserController.php
+++ app/Http/Controllers/PokedexUserController.php
@@ -3,18 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pokemon;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class PokedexUserController extends Controller
 {
-    public function index(){
-        $pokemon = Pokemon::paginate(12);
+    public function index(Request $request){
 
-        $pokemon=Pokemon::with(['type1', 'type2'])->get();
-
-        return view('pokedexuser.index', [
-            'pokemon' => $pokemon,
-        ]);
+        $query = Pokemon::query();
+    if ($request->has('search') && $request->search) {
+        $query->where('name', 'LIKE', '%'.$request->search.'%');
+    }
+    if ($request->has('type') && $request->type) {
+        $query->whereHas('type1', function($q) use ($request) {
+            $q->where('id', $request->type);
+        })->orWhereHas('type2', function($q) use ($request) {
+            $q->where('id', $request->type);
+        });
+    }
+    $pokemon = $query->with(['type1', 'type2'])->get();
+    $types = Type::all();
+    return view('pokedexuser.index', ['pokemon' => $pokemon, 'types' => $types]);
     }
 
 
